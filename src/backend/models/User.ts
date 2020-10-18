4acccafab24d952bb2df8131f9db58a435efe7de
Phillbert Tan <philly@phillytan.xyz>
2020-10-17 21:41:49 -0700

    Revert

--- src/backend/models/User.ts
+++ src/backend/models/User.ts
@@ -1,6 +1,11 @@
-import { Entity, Property, Unique } from '@mikro-orm/core'
+import { Entity, Enum, Property, Unique } from '@mikro-orm/core'
 import { BaseEntity } from './BaseEntity'
 import bcrypt from 'bcrypt'
+
+export enum UserType {
+  COMPANY = 'company',
+  INFLUENCER = 'influencer'
+}
 
 @Entity()
 export class User extends BaseEntity {
@@ -14,12 +19,16 @@ export class User extends BaseEntity {
   @Property()
   password: string
 
+  @Enum()
+  type: UserType
+
   // When I mean user I actually mean the influencer
-  constructor(name: string, email: string, password: string) {
+  constructor(name: string, email: string, password: string, type: UserType = UserType.COMPANY) {
     super()
     this.name = name
     this.email = email
     this.password = bcrypt.hashSync(password, 10)
+    this.type = type
   }
 
   verifyPassword(password: string): boolean {
