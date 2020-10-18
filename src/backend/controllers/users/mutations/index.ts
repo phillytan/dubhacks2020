f41b90f093636b5f4ca744cbb284c72a17c4346b
Christopher Hui <christophergkhui@gmail.com>
2020-10-17 23:20:58 -0700

    more gets

--- src/backend/controllers/users/mutations/index.ts
+++ src/backend/controllers/users/mutations/index.ts
@@ -1,7 +1,14 @@
+import current_user from '../queries/current_user'
+import user from '../queries/user'
+import companies from '../queries/companies'
+import users from '../queries/users'
+
 import create_user from './create_user'
 import login_user from './login_user'
+import add_to_favorite from './add_to_favorite'
 
 export default {
   create_user,
-  login_user
+  login_user,
+  add_to_favorite
 }
